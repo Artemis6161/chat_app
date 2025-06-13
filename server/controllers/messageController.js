77b87c4acc4293fcb53690cf7a95d035a83bc36d
sangeetha <Artemis6161@github.com>
2025-06-14 00:59:36 +0530

    added message controllers

---
+++ server/controllers/messageController.js
@@ -0,0 +1,27 @@
+const Message = require("../models/Message");
+const Chat = require("../models/Chat");
+
+
+const sendMessage = async (req, res) =>{
+const {content, chatId} =req.body;
+if(!content || !chatId){
+    return res.status(400).json({message: "all fields are required"});
+    }
+    
+    try{
+        const newMessage= await Message.create({
+            sender: req.user._id,
+            content,
+            chat:chatId,
+
+        })
+         const populatedMessage = await newMessage.populate("sender", "username email");
+    await populatedMessage.populate("chat");
+    await Chat.findByIdAndUpdate(chatId, { lastMessage: populatedMessage });
+
+    }catch(error){
+         res.status(500).json({ message:"server error", error: err.message});
+    }
+
+}
+module.exports = { sendMessage };

17bd44c2e87bee290af7cbc2f5d89c390dc8d024
sangeetha <Artemis6161@github.com>
2025-06-14 20:53:26 +0530

    Update messageController

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,9 +1,11 @@
-const Message = require("../models/Message");
 const Chat = require("../models/Chat");
+const Message = require("../models/Message");
 
 
 const sendMessage = async (req, res) =>{
+     
 const {content, chatId} =req.body;
+  
 if(!content || !chatId){
     return res.status(400).json({message: "all fields are required"});
     }
@@ -15,25 +17,40 @@ if(!content || !chatId){
             chat:chatId,
 
         })
-         const populatedMessage = await newMessage.populate("sender", "username email");
-    await populatedMessage.populate("chat");
+        
+         const populatedMessage = await Message.findById(newMessage._id)
+  .populate("sender", "username email")
+  .populate({
+    path: "chat",
+    populate: { path: "members", select: "username email" }, // optional: populate chat members
+});
     await Chat.findByIdAndUpdate(chatId, { lastMessage: populatedMessage });
 
+  res.status(201).json(populatedMessage);
+
     }catch(error){
-         res.status(500).json({ message:"server error", error: err.message});
+        console.error("Error sending message:", err);
+         res.status(500).json({ message:"server error", error: error.message});
     }
 
 }
+
+
 const getMessages = async (req, res) => {
-    const {chatId} = req.params;
-    try{
-        const messages = await messages.find({ chat:chatId})
-            .populate("sender", "username email")
-            .populate("chat");
-            res.status(200).json(messages);
+  const { chatId } = req.params;
 
-    } catch(error){
-        res.status(500).json({message: "server error", error: error.message});
-    }
-}
-module.exports = { sendMessage,getMessages };
+  try {
+    const messages = await Message.find({ chat: chatId })
+      .populate("sender", "username email")
+      .populate("chat");
+
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.message });
+  }
+};
+
+
+
+
+module.exports = { sendMessage, getMessages };
